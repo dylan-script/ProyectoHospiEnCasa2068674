4459396cfed56f58ef8a58711620d11b9851c198
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2021-11-15 00:12:35 -0500

    IRepositorioPaciente

---
+++ HospiEnCasa.App/HospiEnCasa.App.Presentacion/IRepositorioPaciente.cs
@@ -0,0 +1,12 @@
+using System;
+using system.collections;
+using HospiEnCasa.App.Dominio;
+namespace HospiEnCasa.App.Persistencia{
+    public interface IRepositorioPaciente{
+        IEnumerable<Paciente> GetAll();
+        Paciente Add(Paciente paciente);
+        Paciente Update(Paciente paciente);
+        void Delete(int id_paciente);
+        Paciente Get(int id_paciente);
+}
+}
