22ab75382db044fffc34bfb6b112a72dc86dfa35
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2021-11-26 23:32:17 -0500

    Medico-Parte 3

--- HospiEnCasa.App/HospiEnCasa.App.Presentacion/Pages/Medicos/Index.cshtml.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Presentacion/Pages/Medicos/Index.cshtml.cs
@@ -1,19 +1,26 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using System.Transactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
 namespace HospiEnCasa.App.Presentacion.Pages_Medicos
 {    
-    private readonly IRepositorioPaciente RepositorioPaciente;
-public IEnumerable<Paciente> Pacientes {get; set;}
-public IndexModel (IRepositorioPaciente RepositorioPaciente){
-    this.RepositorioPaciente = RepositorioPaciente;
-}
-public void OnGet()
-{
-    Pacientes = RepositorioPaciente.GetAll();
-}
+    public class IndexModel : PageModel{
+        private readonly IRepositorioMedico RepositorioMedico;
+        public IEnumerable<Medico> Medicos {get; set;}
+        public IndexModel (IRepositorioMedico RepositorioMedico){
+            this.RepositorioMedico = RepositorioMedico;
+        }
+        public void OnGet()
+        {
+            Medicos = RepositorioMedico.GetAll();
+        }
+    }
 }
