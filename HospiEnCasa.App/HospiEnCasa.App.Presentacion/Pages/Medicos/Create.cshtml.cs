a8e32031a1830b1fdc63376f2b29c6e6984f9125
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2021-12-03 21:48:11 -0500

    Médico - Parte Final (Crear y Listar)

--- HospiEnCasa.App/HospiEnCasa.App.Presentacion/Pages/Medicos/Create.cshtml.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Presentacion/Pages/Medicos/Create.cshtml.cs
@@ -1,32 +1,41 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using System.Transactions;
 using HospiEnCasa.App.Dominio;
 using HospiEnCasa.App.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
- 
+
 namespace HospiEnCasa.App.Presentacion.Pages_Medicos
 {
-    public class IndexModel : PageModel
+    public class CreateModel : PageModel
     {
         private readonly IRepositorioMedico RepositorioMedico;
 
-        public IEnumerable<Medico> Medicos { get; set; }
-
-        public IndexModel(IRepositorioMedico RepositorioMedico)
+        public CreateModel(IRepositorioMedico RepositorioMedico)
         {
             this.RepositorioMedico = RepositorioMedico;
         }
 
         public void OnGet()
         {
-            Medicos = RepositorioMedico.GetAll();
+        }
+
+        [BindProperty]
+        public Medico Medico { get; set; }
+
+        public IActionResult OnPostSave()
+        {
+            if (ModelState.IsValid)
+            {
+                RepositorioMedico.Add (Medico);
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
